87b9d1e5deae3bc97333021a84d98b356de64a3a
Jhon-Jesus <jhuarcayac@unjbg.edu.pe>
2025-06-25 22:03:52 -0500

    Mejoramiento de la interfaz para el usuario, ademas de una corrección de un detalle en la logica del programa

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -10,15 +10,22 @@ struct EMP{
 int main(){
     EMP EMPLEADOS[100];
     int n;
+    cout<<"Introduzca el numero total de empleados a registrar: ";
     cin>>n;
 
     for(int i=0;i<n;i++){
+    cout<<"\nIntroduzca el numero de identificacion del empleado: ";
     cin>>EMPLEADOS[i].num;
     cin.ignore();
+    cout<<"Introduzca el nombre del empleado: ";
     getline(cin,EMPLEADOS[i].nom);
 
+
+    cout<<"Introduzca el salario del empleado: ";
+    cin>>EMPLEADOS[i].sal;
     for(int j=0;j<12;j++){
-        cin>>EMPLEADOS[i].sal;
+
+        cout<<"Introduzca la venta obtenida de el: ";
         cin>>EMPLEADOS[i].ven[j];
     }
 
