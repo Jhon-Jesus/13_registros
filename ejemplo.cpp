9a89a4f492e7183bf58bff493d7e62ea4e559f24
Jhon-Jesus <jhuarcayac@unjbg.edu.pe>
2025-06-25 23:42:14 -0500

    Listado del empleado que tuvo la mayor cantidad de ventas durante el año

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -9,7 +9,9 @@ struct EMP{
 
 int main(){
     EMP EMPLEADOS[100];
-    int n;
+    int n, maximo=0;
+    float maxven;
+
     cout<<"Introduzca el numero total de empleados a registrar: ";
     cin>>n;
 
@@ -20,16 +22,39 @@ int main(){
     cout<<"Introduzca el nombre del empleado: ";
     getline(cin,EMPLEADOS[i].nom);
 
-
-    cout<<"Introduzca el salario del empleado: ";
-    cin>>EMPLEADOS[i].sal;
     for(int j=0;j<12;j++){
-
         cout<<"Introduzca la venta obtenida de el: ";
         cin>>EMPLEADOS[i].ven[j];
+        cout<<"Introduzca el salario del empleado: ";
+        cin>>EMPLEADOS[i].sal;
     }
 
     }
+
+      for (int i=0; i<n; i++) {
+        float totven = 0;
+        for (int j=0; j<12; j++) {
+            totven += EMPLEADOS[i].ven[j];
+        }
+        if (totven>maxven) {
+            maxven=totven;
+            maximo=i;
+        }
+        cout<<"--------------------------------------------------\n";
+        cout<<"Empleado "<<EMPLEADOS[i].num<<": "<<EMPLEADOS[i].nom<<endl;
+        cout<<"Ventas mensuales: ";
+        for (int j=0; j<12; j++) {
+            cout<<EMPLEADOS[i].ven[j]<<" ";
+        }
+        cout<<"\nTotal ventas: "<<totven<<endl;
+
+    }
+
+    cout<<"\nEmpleado con mas ventas:"<<endl;
+    cout<<"Numero: "<<EMPLEADOS[maximo].num<<endl;
+    cout<<"Nombre: "<<EMPLEADOS[maximo].nom<<endl;
+
+
 
     return 0;
 }
