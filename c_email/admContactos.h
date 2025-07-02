6a4bd81aaad5031d176157123eff1f3fd006ff88
Jhon-Jesus <jhuarcayac@unjbg.edu.pe>
2025-07-01 23:59:59 -0500

    Modularizacion del archivo contactos.cpp

---
+++ c_email/admContactos.h
@@ -0,0 +1,11 @@
+#ifndef ADMCONTACTOS_H
+#define ADMCONTACTOS_H
+#include "contacto.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void agregarContacto(contactoEmail [], contactoEmail, int &);
+void mostrarContactos(contactoEmail [], int);
+
+#endif
---
+++ c_email/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include "contacto.h"
+#include "admContactos.h"
+#define MAX 10
+using namespace std;
+
+int main(){
+    int n, op;
+    string nom,user, domain;
+    char sex, SN;
+    int edad;
+    correo email;
+    contactoEmail cont, lista[100];
+    n=0;
+
+   do{
+        system("cls");
+        cout<<"Menu de opciones -------------------------"<<endl;
+        cout<<"1. Agregar contacto"<<endl;
+        cout<<"2. Mostrar contactos"<<endl;
+        cout<<"3. Modificar contacto"<<endl;
+        cout<<"0. Salir"<<endl;
+        cout<<"Elige una opcion: "; cin>>op;
+        switch(op){
+            case 1:
+                cout<<"Ingrese los datos de un usuario: "<<endl;
+                cin.ignore();
+                cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
+                cout<<"Ingrese el sexo (M/F): "; cin>>sex;
+                cout<<"Ingrese la edad: "; cin>>edad;
+                cout<<"Ingrese el correo electronico (usuario@dominio): "<<endl;
+                cout<<"\tIngrese el usuario: "; cin>>user;
+                cout<<"\tIngrese el dominio: "; cin>>domain;
+
+                leerCorreo(email,user,domain);
+                leerContacto(cont,nom,sex,edad,email);
+                //imprimeContacto(cont);
+
+                lista[n] = cont;
+                n++;
+                system("pause");
+                break;
+            case 2:
+                for(int i = 0; i < n; i++){
+                    cout<<"Contacto #"<<i<<endl;
+                    imprimeContacto(lista[i]);
+                    cout<<endl;
+                }
+                system("pause");
+                break;
+            case 3:
+            if(n==0){
+            cout<<"No hay contactos para modificar, ingrese primero un contacto"<<endl;
+            }
+            break;
+
+
+            case 0:
+                cout<<"Esta seguro de salir? (S/N): ";
+
+                break;
+            default:
+                cout<<"Opcion no valida!"<<endl;
+                system("pause");
+                break;
+        }
+    } while(op != 0);
+    return 0;
+
+}
